ad2e29f2fbab103ab2bcfa9dbcb8ad0f586daab0
Kunal Mukherjee <kunalm.8470@outlook.com>
2021-12-22 21:16:05 +0530

    Added example for Azure Key Vault

---
+++ key-vault/src/Api/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(IConfiguration configuration, ILogger<ValuesController> logger)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string secret = _configuration.GetValue<string>("Secret");
+            _logger.LogInformation("Secret from Azure Key Vault: {0}", secret);
+            return Ok(secret);
+        }
+    }
+}
